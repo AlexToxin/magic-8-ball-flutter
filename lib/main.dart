b0c472d39a16ffb4463b020dcd5d5b7b7038af59
AlexToxin <avesuslov@gmail.com>
2020-04-09 18:16:17 +0400

    Stateful Ball + onPressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -20,9 +22,30 @@ class BallPage extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.all(25),
         child: Center(
-          child: Image.asset('images/ball1.png'),
+          child: Ball(),
         ),
       ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {
+        setState(() {
+          ballNumber = Random().nextInt(4) + 1;
+        });
+      },
+      child: Image.asset('images/ball$ballNumber.png'),
     );
   }
 }
