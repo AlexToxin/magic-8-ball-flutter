b3a875c405d4195b9b0edceb10c99cbce339f29c
AlexToxin <avesuslov@gmail.com>
2020-04-09 18:11:39 +0400

    Default markup

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: SafeArea(
+          child: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(25),
+        child: Center(
+          child: Image.asset('images/ball1.png'),
+        ),
+      ),
+    );
+  }
+}
